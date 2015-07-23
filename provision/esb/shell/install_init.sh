7846eefefb0f4b81538b9df8c4c5ee1869670814
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-07-23 15:51:50 +0100

    Update install_init.sh

--- provision/esb/shell/install_init.sh
+++ provision/esb/shell/install_init.sh
@@ -7,3 +7,5 @@ yum install zip unzip maven activemq java-1.7.0-openjdk
 echo -e "\n\nJAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk" >> /etc/environment;
 export JAVA_HOME="/etc/alternatives/java_sdk_1.7.0_openjdk"
 
+sudo service firewalld stop
+
