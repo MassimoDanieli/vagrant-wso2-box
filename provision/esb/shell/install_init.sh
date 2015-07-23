a30dc2b2853b4731724dd48a3b60422313569a6e
Massimo Danieli <andy@nano.local>
2015-07-23 16:44:34 +0100

    added puppet to istall_init.sh file

--- provision/esb/shell/install_init.sh
+++ provision/esb/shell/install_init.sh
@@ -2,7 +2,7 @@
 # install jdk 7#!/bin/bash
 yum update -y
 
-yum install zip unzip maven activemq java-1.7.0-openjdk
+yum install zip unzip maven activemq java-1.7.0-openjdk puppet
 
 echo -e "\n\nJAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk" >> /etc/environment;
 export JAVA_HOME="/etc/alternatives/java_sdk_1.7.0_openjdk"
