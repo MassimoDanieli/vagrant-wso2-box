ecf376f8d02d383c126eb724cc00dff5c1adc14b
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-07-23 15:50:50 +0100

    switechd from apt-get to YUM

--- provision/esb/shell/install_init.sh
+++ provision/esb/shell/install_init.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 # install jdk 7#!/bin/bash
-apt-get update
-apt-get -y upgrade
-apt-get autoremove
-apt-get -y install python-software-properties
-add-apt-repository -y ppa:webupd8team/java
-apt-get -q -y update
+yum update -y
 
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+yum install zip unzip maven activemq java-1.7.0-openjdk
 
-apt-get -y install oracle-java7-installer
+echo -e "\n\nJAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk" >> /etc/environment;
+export JAVA_HOME="/etc/alternatives/java_sdk_1.7.0_openjdk"
 
-echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
-
-# install maven
-apt-get -q -y install maven
-
-# install activemq
-apt-get -q -y install activemq
-
-# install svn
-apt-get -y install subversion
