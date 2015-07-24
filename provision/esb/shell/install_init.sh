327a1f30bf4122b5cd8bbe380ff716a447847000
Massimo Danieli <andy@nano.local>
2015-07-24 11:08:29 +0100

    typo on yum install line

--- provision/esb/shell/install_init.sh
+++ provision/esb/shell/install_init.sh
@@ -4,7 +4,7 @@
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum update -y
 
-yum install zip unzip maven activemq java-1.7.0-openjdk puppet
+yum install -y zip unzip maven activemq java-1.7.0-openjdk puppet
 
 echo -e "\n\nJAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk" >> /etc/environment;
 export JAVA_HOME="/etc/alternatives/java_sdk_1.7.0_openjdk"
