794c1e8f552ca386db63d89b23d826b0149b7ab7
Massimo Danieli <andy@nano.local>
2015-07-24 09:15:53 +0100

    added puppet: permissive SELINUX

--- provision/esb/shell/install_init.sh
+++ provision/esb/shell/install_init.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # install jdk 7#!/bin/bash
+
+sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum update -y
 
 yum install zip unzip maven activemq java-1.7.0-openjdk puppet
 
 echo -e "\n\nJAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk" >> /etc/environment;
 export JAVA_HOME="/etc/alternatives/java_sdk_1.7.0_openjdk"
-
+sed -i -e 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
 sudo service firewalld stop
 
