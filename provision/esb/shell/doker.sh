7a54d40501045e3b17104db7dd75cb123e01165b
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-07-30 09:09:03 +0100

    Create doker.sh

---
+++ provision/esb/shell/doker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum update -y
+curl -sSL https://get.docker.com/ | sh
+sudo service docker start
+docker run --rm --name test-dev-am -p 9445:9443 massimodanieli/wso2apim
+docker run --rm --name test-andy -p 9443:9443 massimodanieli/wso2ebs
