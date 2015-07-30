963893c7bd88f557980538d50c982df20ca2fe0b
MassimoDanieli <massimo.danieli@openeyes.org.uk>
2015-07-30 10:39:05 +0100

    Update docker.sh

--- provision/esb/shell/docker.sh
+++ provision/esb/shell/docker.sh
@@ -4,6 +4,6 @@
 curl -sSL https://get.docker.com/ | sh
 sudo usermod -aG docker vagrant
 sudo service docker start
-docker run --rm --name test-dev-am -p 9445:9443 massimodanieli/wso2apim /dev/null 2>&1
-docker run --rm --name test-dev-ebs -p 9443:9443 massimodanieli/wso2esb /dev/null 2>&1
-docker run --rm --name test-dev-dss -p 9444:9443 massimodanieli/wso2dss /dev/null 2>&1
+docker run -d --name test-dev-am -p 9445:9443 massimodanieli/wso2apim 
+docker run -d --name test-dev-ebs -p 9443:9443 massimodanieli/wso2esb 
+docker run -d --name test-dev-dss -p 9444:9443 massimodanieli/wso2dss 
